74795fe2218565c4f6c13040906509e7c6cdd3cb
Khal <ailoor.11s@gmail.com>
2024-11-12 16:54:50 +0100

    consumer

---
+++ kafka_consumer.py
@@ -0,0 +1,17 @@
+from kafka import KafkaConsumer
+import json
+
+# Configurer le consommateur Kafka
+consumer = KafkaConsumer(
+    'sales_topic',  # Le nom du topic auquel vous souhaitez vous abonner
+    bootstrap_servers='localhost:9092',  # Use the correct Kafka broker address
+    group_id='sales_group',  # L'ID du groupe de consommateurs
+    auto_offset_reset='earliest',  # Start reading from the beginning if no offset exists
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+)
+
+# Consommer les messages
+for message in consumer:
+    sale_event = message.value  # Le message consommé
+    print(f"Événement reçu: {sale_event}")
+    # Vous pouvez ajouter ici la logique pour traiter les données, comme les enregistrer dans une base de données
--- kafka_producer.py
+++ kafka_producer.py
@@ -5,7 +5,7 @@ import time
 
 # Configurer le producteur Kafka
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9093',
+    bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
